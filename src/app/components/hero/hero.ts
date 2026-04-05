1aa0fb50f05703e875cdade9b8c511f6485d2eb5
wenchispop <debo.palma@duocuc.cl>
2026-04-05 18:42:15 -0400

    Update UI visual
    Update UI visual

--- src/app/components/hero/hero.ts
+++ src/app/components/hero/hero.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Navbar } from '../navbar/navbar';
+import { Footer } from '../footer/footer';
 
 @Component({
   selector: 'app-hero',
-  imports: [Navbar],
+  imports: [Navbar, Footer],
   templateUrl: './hero.html',
   styleUrl: './hero.css',
 })
